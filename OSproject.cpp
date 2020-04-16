81a962306942ff3f2aea0c253b0a3b5080cbdd0a
Rock4244 <51767654+Rock4244@users.noreply.github.com>
2020-04-16 20:52:17 +0530

    Update OSproject.cpp

--- OSproject.cpp
+++ OSproject.cpp
@@ -1,65 +1,54 @@
-#include<Pthread.h> //for thread funtion
-#include<studio.h>
-void *s1()
+#include<stdio.h>
+#include<stdbool.h>
+struct requirement
 {
-	int pen;
-	pthread_mutex_lock(&l);
-	sem_wait(&s);
-	pen=t;
-	pen=pen+paper+questionpaper;
-	t=paper;
-	sem_post(&s);
-	
-	
-}
-void *s2()
-{
-	int questionpaper;
-	pthread_mutex_lock(&l);
-	sem_wait(&s);
-	questionpaper=t;
-	questionpaper=questionpaper+paper+pen;
-	sleep(2);
-	t=questionpaper;
-	sem_post(&s);
-}
-void *s3()
-{
-    int paper;
-	pthread_mutex_lock(&l);	
-    sem_wait(&S);
-    paper=t;
-    paper=paper+pen+questionpaper;
-    t=paper;
-    sem_post(&s);
-}
-void *t()
-{
-	
-	int pen;
-	int paper;
-	int question paper;
-}
+ 	bool pen ;
+	bool paper ;
+	bool question_paper ;
+	bool all_three ;
+};
 int main()
 {
-	sem_init(&s,0,1);
-	pthread_mutex_init(&l,NULL)
-	pthread_t s1,s2,s3,t1;
-	pthread_create(&s1,NULL,&fs1,NULL);
-	pthread_join(&s1,NULL);
-	pthread_create(&s2,NULL,fs2,NuLL);
-	pthread_join(&s2,NULL);
-	pthread_create(&s3,NULL,fs3,NULL);
-	pthread_join(&s3,NULL);
-	pthread_create(&t,NULL,ft,NULL);
-	pthread_join(&t,NULL);
-	
-	
+	int n=3;
+	struct requirement  s[n];
+	s[0].pen=true;		
+	s[0].paper = false;
+	s[0].question_paper = false;
+	s[0].all_three= false;
+	s[1].pen=false;		
+	s[1].paper = true;
+	s[1].question_paper = false;
+	s[1].all_three = false;
+	s[2].pen=false;		
+	s[2].paper = false;
+	s[2].question_paper = true;
+	s[2].all_three = false	;
+	while(s[0].all_three==false||s[1].all_three==false||s[2].all_three==false)
+	{
+		int ch1,ch2;
+		printf("\nResources:\n1.pen\n2.paper\n3.question paper\n Enter the two things which is to be placed on the shared table: ");
+		scanf("%d%d",&ch1,&ch2);
+		if(ch1==1 && ch2==2 && s[2].all_three==false)
+		{
+			s[2].all_three=true ;
+			printf("Third Student has completed the task\n");
+		}
+		if(ch1==2 && ch2==3 && s[0].all_three==false)
+		{
+			s[0].all_three=true;
+			printf("First Student has completed the task\n");
+		}
+		if(ch1==1 && ch2==3 && s[1].all_three==false)
+			while(s[0].all_three==false||s[1].all_three==false||s[2].all_three==false)
+	{
+		if(ch1==1 && ch2==3 && s[1].all_three==false)
+		{
+			s[1].all_three=true;
+			printf("Second Student has completed the task\n");
+		}
+	}
+	printf("All the students now have completed their respective tasks succesfully\n");
+	return 0;
 }
-void s1();
-void s2();
-void s3();
-void t1();
-sem_t s;
-int t;
-pthread_mutex_t l;
+
+
